833f7f00f29f70207cb515da9aec129967519a39
Сизиков Семён <Tyuiu.SizikovSS@outlook.com>
2024-12-08 19:40:54 +0500

    Task4

--- Tyuiu.SizikovSS.Sprint3.Task4.V27.Lib/DataService.cs
+++ Tyuiu.SizikovSS.Sprint3.Task4.V27.Lib/DataService.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.SizikovSS.Sprint3.Task4.V27.Lib
             double y = 0;
             for (int x = startValue; x <= stopValue; x++)
             {
-                if (x == 0) break;
+                if (x == 0) continue;
                 else y += x / (Math.Cos(x) + Math.Sin(x));
             }
             return Math.Round(y,3);
