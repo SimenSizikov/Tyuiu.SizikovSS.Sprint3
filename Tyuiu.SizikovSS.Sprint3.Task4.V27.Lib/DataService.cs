48a323f4485f4ccbf7419f54e26f00e8d4714e35
Сизиков Семён <Tyuiu.SizikovSS@outlook.com>
2024-12-08 19:38:46 +0500

    Task4

--- Tyuiu.SizikovSS.Sprint3.Task4.V27.Lib/DataService.cs
+++ Tyuiu.SizikovSS.Sprint3.Task4.V27.Lib/DataService.cs
@@ -7,14 +7,12 @@ namespace Tyuiu.SizikovSS.Sprint3.Task4.V27.Lib
         public double Calculate(int startValue, int stopValue)
         {
             double y = 0;
-            double res = 0;
             for (int x = startValue; x <= stopValue; x++)
             {
                 if (x == 0) break;
-                else y = x / (Math.Cos(x) + Math.Sin(x));
-                res += y;
+                else y += x / (Math.Cos(x) + Math.Sin(x));
             }
-            return Math.Round(y);
+            return Math.Round(y,3);
         }
     }
 }
--- Tyuiu.SizikovSS.Sprint3.Task4.V27.Test/DataServiceTest.cs
+++ Tyuiu.SizikovSS.Sprint3.Task4.V27.Test/DataServiceTest.cs
@@ -8,6 +8,10 @@ namespace Tyuiu.SizikovSS.Sprint3.Task4.V27.Test
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds = new();
+            int a = -5, b = 5;
+            var res = ds.Calculate(a, b);
+            Assert.AreEqual(-44.313, res);
         }
     }
 }
